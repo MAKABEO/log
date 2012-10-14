364700034a81d79323eebe1aff912ef2b9f33149
Jesus Antonio Villarraga Rojas <tonokong@hotmail.com>
2012-10-13 20:57:54 -0500

    Mi primer repositorio

---
+++ datos/funcs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+public static class Informacion
+{
+    public const String[] nombres = {makabeo,zhionit,zheref,batman,suphavanegas,frenterodilla,toño,mulk,lachole,elbarto};
+    public const String[] claves = {0924,sergio,alatairis,darks,motorhead,calvo,chucho,verde,naranja,jijijiji};
+
+    public Boolean IsUsuario(var nombre, var clave)
+    {
+        Boolean isEncontrado = false;
+        for(int i = 0; i < nombres.Length; i++)
+        {
+            if(nombres[i] == nombre && claves[i] == clave)
+            {
+                isEncontrado = true;
+            }
+            else
+            {
+                isEncontrado = false;
+            }
+        }
+        return isEncontrado;
+    }
+}
+
