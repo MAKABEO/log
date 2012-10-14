01156140e6829c324ad19281015747c356bbc618
MAKABEO <tonokong@hotmail.com>
2012-10-13 21:25:02 -0500

    Update datos/funcs.cs

--- datos/funcs.cs
+++ datos/funcs.cs
@@ -4,8 +4,8 @@ using System.Web;
 
 public static class Informacion
 {
-    public const String[] nombres = {makabeo,zhionit,zheref,batman,suphavanegas,frenterodilla,toño,mulk,lachole,elbarto};
-    public const String[] claves = {0924,sergio,alatairis,darks,motorhead,calvo,chucho,verde,naranja,jijijiji};
+    public const String[] nombres = {"makabeo","zhionit","zheref","batman","suphavanegas","frenterodilla","toño","mulk","lachole","elbarto"};
+    public const String[] claves = {"0924","sergio","alatairis","darks","motorhead","calvo","chucho","verde","naranja","jijijiji"};
 
     public Boolean IsUsuario(var nombre, var clave)
     {
